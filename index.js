390d03a89a54e746dbede19af90b2705b1ad64a2
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-04 16:46:22 +0200

    Random sign

---
+++ index.js
@@ -0,0 +1,30 @@
+function onButtonClick(signeplayer) {
+    var min = 1
+    var max = 3
+    var signe_ia = ""
+    var random = Math.floor(Math.random() * (max - min + 1) + min)
+    var imageplayer = document.getElementById("player-signe")
+    var imageia = document.getElementById("ia-signe")
+    if (signeplayer === "pierre") {
+        imageplayer.setAttribute("src", "images/1-pierre.jpg")
+    }
+    if (signeplayer === "feuille") {
+        imageplayer.setAttribute("src", "images/2-feuille.jpg")
+    }
+    if (signeplayer === "ciseaux") {
+        imageplayer.setAttribute("src", "images/3-ciseaux.jpg")
+    }
+    if (random === 1) {
+        imageia.setAttribute("src", "images/1-pierre.jpg")
+        signe_ia = "pierre"
+    }
+    if (random === 2) {
+        imageia.setAttribute("src", "images/2-feuille.jpg")
+        signe_ia = "feuille"
+    }
+    if (random === 3) {
+        imageia.setAttribute("src", "images/3-ciseaux.jpg")
+        signe_ia ="ciseaux"
+    }
+    
+}
