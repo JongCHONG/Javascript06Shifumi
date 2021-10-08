13b8bbfb96ff8bec4e8093cf86e00ea980187a13
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-08 16:03:29 +0200

    Shifumi v2

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var score_ia = 0
 var score_player = 0
 var image_ia = document.getElementById("ia-signe")
+var image_player = document.getElementById("player-signe")
 var signe_ia = ""
+var restart = false
 
 function onButtonClick(signe_player) {
     var min = 1
     var max = 3
     var random = Math.floor(Math.random() * (max - min + 1) + min)
-    var image_player = document.getElementById("player-signe")
     document.getElementById("details").innerHTML = ''
-
+    if (restart == true) {
+        document.getElementById("historique").innerHTML = ''
+        restart = false
+        console.log("hello");
+    }
     if (signe_player === "pierre") {
         image_player.setAttribute("src", "images/1-pierre.png")
     }
@@ -32,10 +37,10 @@ function onButtonClick(signe_player) {
             (signe_player === "ciseaux" && signe_ia === "feuille") ||
             (signe_player === "pierre" && signe_ia === "ciseaux")) {
                 score_player++
-                var details = document.createTextNode("Player wins")
+                var details = document.createTextNode("Avenger wins")
             } else {
                 score_ia++
-                var details = document.createTextNode("IA wins")
+                var details = document.createTextNode("Ultron wins")
             }
         var round = document.createElement("p")
         round.appendChild(details)
@@ -43,24 +48,28 @@ function onButtonClick(signe_player) {
         element.appendChild(round)
         //affichage round
         var resultat = document.createElement("h2")
-        var text = document.createTextNode(`${score_ia} IA - Moi ${score_player}`)
+        var text = document.createTextNode(`${score_ia} Ultron - Avenger ${score_player}`)
         resultat.appendChild(text)
-        var element = document.getElementById("result")
+        var element = document.getElementById("historique")
         element.appendChild(resultat)
         document.getElementById("score-ia").innerHTML = "- " + score_ia
         document.getElementById("score-player").innerHTML = "- " + score_player
         //Bonus Score
         var resultatfinal = document.createElement("p")
         var final 
-        if (score_ia === 5) {
-            final = document.createTextNode(`${score_ia} IA gagne la partie`)
+        if (score_ia === 3) {
+            restart = true
+            document.getElementById("details").innerHTML = `${score_ia} - ${score_player}`
+            final = document.createTextNode(`Ultron gagne la partie`)
             score_ia = 0
             score_player = 0
             resultatfinal.appendChild(final)
             var element = document.getElementById("details")
             element.appendChild(resultatfinal)
-        } else if (score_player === 5) {
-            final = document.createTextNode(`${score_player} Player gagne la partie`)
+        } else if (score_player === 3) {
+            restart = true
+            document.getElementById("details").innerHTML = `${score_ia} - ${score_player}`
+            final = document.createTextNode(`Avengers gagne la partie`)
             score_player = 0
             score_ia = 0
             resultatfinal.appendChild(final)
@@ -88,7 +97,12 @@ function random_ia () {
 }
 // pour effacer le contenu d'un html
 function clearResult(){
-    document.getElementById("result").innerHTML = '' 
+    document.getElementById("details").innerHTML = ''
+    document.getElementById("historique").innerHTML = ''
     score_player = 0
     score_ia = 0
+    document.getElementById("score-ia").innerHTML = ""
+    document.getElementById("score-player").innerHTML = ""
+    image_ia.setAttribute("src", "")
+    image_player.setAttribute("src", "")
 }
